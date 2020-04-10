90d78258f096c82a94f3e5350c923048057bb594
alexriss <00alexx@riss.at>
2020-04-10 17:15:15 +0200

    new build

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/SARS-CoV-2-Analysis/'
-        : '/',
-    transpileDependencies: [
-        'vue-echarts',
-        'resize-detector'
-    ]
-}
